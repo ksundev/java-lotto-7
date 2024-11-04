09ed5e9f09d82b435cca6f05e736c3103d9435e1
ksundev <kshk08@hanyang.ac.kr>
2024-11-04 21:20:33 +0900

    feat(GameContext): 금액 입력 및 검증 로직 추가
    GameContext 클래스에 금액 입력 및 검증 메서드를 추가하였습니다.
    입력 값은 null, 비숫자, 음수, 1000원 단위 검사를 포함합니다.
    이제 Application 클래스에서 game.start()를 통해 입력을 받아 게임을 시작할 수 있습니다.

---
+++ src/main/java/lotto/GameContext.java
@@ -0,0 +1,35 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameContext {
+    private int budget;
+
+    public GameContext() {
+    }
+
+    public void start() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String inputBudget = Console.readLine();
+        budget = parseIntBudget(inputBudget);
+    }
+
+    int parseIntBudget(String inputBudget) {
+        if (inputBudget == null) {
+            throw new IllegalArgumentException("[ERROR] 입력 값이 비어있습니다.");
+        }
+        int budget;
+        try {
+            budget = Integer.parseInt(inputBudget);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 숫자(정수) 형식으로 입력해주세요.");
+        }
+        if (budget < 0) {
+            throw new IllegalArgumentException("[ERROR] 음수는 입력할 수 없습니다.");
+        }
+        if (budget % 1000 != 0) {
+            throw new IllegalArgumentException("[ERROR] 1000원 단위로 입력해주세요.");
+        }
+        return budget;
+    }
+}
