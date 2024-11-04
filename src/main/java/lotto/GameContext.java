780f882122de92ba862f9a0884a6538a964ebb38
ksundev <kshk08@hanyang.ac.kr>
2024-11-04 22:56:58 +0900

    feat(GameContext): 발행 개수에 맞게 Lotto를 발행하고 출력하는 기능 추가

--- src/main/java/lotto/GameContext.java
+++ src/main/java/lotto/GameContext.java
@@ -11,8 +11,10 @@ import java.util.Set;
 public class GameContext {
     private int budget;
     private int lottoQuantity;
+    private List<Lotto> lottos;
 
     public GameContext() {
+        lottos = new ArrayList<>();
     }
 
     public void start() {
@@ -20,6 +22,14 @@ public class GameContext {
         String inputBudget = Console.readLine();
         budget = parseIntBudget(inputBudget);
         lottoQuantity = budget / 1000;
+    }
+
+    public void progress() {
+        for (int i = 0; i < lottoQuantity; i++) {
+            Lotto lotto = new Lotto(makeLottoNumbers());
+            lottos.add(lotto);
+        }
+        printLottos();
     }
 
     int parseIntBudget(String inputBudget) {
@@ -51,5 +61,11 @@ public class GameContext {
             numberSet.add(Randoms.pickNumberInRange(1, 45));
         }
         return new ArrayList<>(numberSet);
+    }
+
+    public void printLottos() {
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto.toString());
+        }
     }
 }
