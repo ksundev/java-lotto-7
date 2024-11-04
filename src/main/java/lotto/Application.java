b9f99be51dcf5fbf114fe48352421d787bbf30c3
ksundev <kshk08@hanyang.ac.kr>
2024-11-04 22:41:32 +0900

    feat(Application): 발행 수 출력 기능 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -4,5 +4,6 @@ public class Application {
     public static void main(String[] args) {
         GameContext game = new GameContext();
         game.start();
+        System.out.printf("\n%d개를 구매했습니다.\n", game.getLottoQuantity());
     }
 }
